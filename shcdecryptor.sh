18f0b20102beefe32c0968e7402afef8469a27cb
Wildy Sheverando <shiwildy@gmail.com>
2024-10-31 12:53:59 +0700

    feat: add file not found handle

--- shcdecryptor.sh
+++ shcdecryptor.sh
@@ -2,11 +2,17 @@
 
 if [[ $1 == "--installation-testing" ]]; then
   echo "ok"
+  exit
 fi
 
 for args in $@; do
+  if ! [[ -f $args ]]; then
+    echo "$args not found."
+    exit
+  fi
+
   $args > /dev/null & child=$!
-  sleep 0.0.3
+  sleep 0.03
   kill -STOP $child
   cat /proc/$child/cmdline | sed 's/.*\(#!\)/\1/; $d' > $args.decrypted.sh
   kill -TERM $child
